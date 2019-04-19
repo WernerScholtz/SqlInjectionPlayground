deb6ad1d3b94d38fddecdfd62945b262ee0d39d3
Werner Scholtz <werner.scholtz@entelect.co.za>
2019-04-19 15:32:20 +0200

    Get rid of unused code and views

--- SqlInjectionDemo/Controllers/HomeController.cs
+++ SqlInjectionDemo/Controllers/HomeController.cs
@@ -55,16 +55,5 @@ namespace SqlInjectionDemo.Web.Public.Controllers
                 return View(model);
             }
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
